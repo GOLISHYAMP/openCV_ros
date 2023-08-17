1abba5f29023a035f0299ab4071d122f7d9e13df
Shyam Goli <golishyamgolisai@gmail.com>
2023-08-17 22:59:00 +0530

    change in drawing_shapes

--- src/my_open_cv_codes/scripts/drawing_shapes.py
+++ src/my_open_cv_codes/scripts/drawing_shapes.py
@@ -4,7 +4,7 @@ import cv2
 img = np.zeros((512,512,3), np.uint8)
 
 cv2.line(img,(0,0),(511,511),(255,255,255),5)
-cv2.line(img,(0,511),(511,0),(255,255,255),5)
+#cv2.line(img,(0,511),(511,0),(255,255,255),5)
 font = cv2.FONT_HERSHEY_SIMPLEX
 cv2.putText(img,'Learning ROS OpenCV',(10,250),font, 1, (255,255,255),2, cv2.LINE_AA)
 cv2.imshow("Image",img)
